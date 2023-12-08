427389f4bc627054821e5cc3f9004df619c79222
Cristi Olteanu <cristiolteanu1892@gmail.com>
2023-12-08 16:03:23 +0200

    added delete and edit items

--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 
@@ -15,10 +15,10 @@ def detail(request, pk):
     return render(request, 'item/detail.html', context)
 
 
-@login_required()
+@login_required
 def new(request):
     if request.method == "POST":
-        form = NewItemForm(request.POST, request.FILES)
+        form = EditItemForm(request.POST, request.FILES)
 
         if form.is_valid():
             item = form.save(commit=False)
@@ -30,4 +30,31 @@ def new(request):
         form = NewItemForm()
 
     return render(request, 'item/form.html', {"form": form})
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
 
